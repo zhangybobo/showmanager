926dd2ceebc3903d2c34d090548cdf631e766e23
zyb <zyb@example.com>
2019-02-13 17:40:23 +0800

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- <el-form :label-position="labelPosition" label-width="80px" :model="formdata">
+            <el-form-item label="姓名">
+              <el-input v-model="formLabelAlign.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input v-model="formLabelAlign.region"></el-input>
+            </el-form-item>
+            
+        </el-form> -->
+        login---
+    </div>
+</template>
+
+<script>
+    export default {
+      // data(){
+      //   return {
+      //  labelPosition:'top',
+      //  formdata:{
+      //    username:'',
+      //    password:''
+      //  }
+      //   }
+      // }
+    }
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import login from '@/components/Login.vue'
+import home from '@/components/home.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component:home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component:login
     }
   ]
 })
